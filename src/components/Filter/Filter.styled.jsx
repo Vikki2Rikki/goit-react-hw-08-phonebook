c9309ecfcab040251165c6e36a2ef6feacb6c339
Vikki2Rikki <119513480+Vikki2Rikki@users.noreply.github.com>
2023-08-27 19:54:11 +0300

    up

--- src/components/Filter/Filter.styled.jsx
+++ src/components/Filter/Filter.styled.jsx
@@ -5,19 +5,15 @@ export const FilterWrap = styled.div`
   flex-direction: column;
   gap: 5px;
   margin-top: 15px;
-  padding-left: 30px;
-  padding-right: 150px;
-  padding-bottom: 15px;
+  align-items: center;
 `;
 
 export const FilterLabel = styled.label`
   font-weight: 500;
-  margin-left: 450px;
+
   margin-bottom: 20px;
 `;
 export const FilterInput = styled.input`
   display: block;
   width: 300px;
-  margin-left: 450px;
-  /* margin: 0 auto; */
 `;
